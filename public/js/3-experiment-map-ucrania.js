9e08622f41887e4854556bb7045612281aacc9e3
sucendo <samuelug.pro@gmail.com>
2022-03-03 16:33:04 +0100

    Update 3-experiment-map-ucrania.js

--- public/js/3-experiment-map-ucrania.js
+++ public/js/3-experiment-map-ucrania.js
@@ -5,7 +5,7 @@
         zoom: 5,
         pitch: 50,
         bearing: 0,
-        style: 'mapbox://styles/mapbox-map-design/ckhqrf2tz0dt119ny6azh975y'
+        style: 'mapbox://styles/mapbox/outdoors-v11'
     });
 
     map.on('load', () => {
@@ -28,6 +28,40 @@
         'sky-atmosphere-sun-intensity': 15
         }
         });
+      
+      map.addLayer({
+'id': '3d-buildings',
+'source': 'composite',
+'source-layer': 'building',
+'filter': ['==', 'extrude', 'true'],
+'type': 'fill-extrusion',
+'minzoom': 15,
+'paint': {
+'fill-extrusion-color': '#aaa',
+ 
+// use an 'interpolate' expression to add a smooth transition effect to the
+// buildings as the user zooms in
+'fill-extrusion-height': [
+'interpolate',
+['linear'],
+['zoom'],
+15,
+0,
+15.05,
+['get', 'height']
+],
+'fill-extrusion-base': [
+'interpolate',
+['linear'],
+['zoom'],
+15,
+0,
+15.05,
+['get', 'min_height']
+],
+'fill-extrusion-opacity': 0.6
+}
+});
 
         map.addSource('national-park', {
             'type': 'geojson',

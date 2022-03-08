5d2c03edb9be357c0c179ab888691b65430dc309
sucendo <samuelug.pro@gmail.com>
2022-03-08 16:28:32 +0100

    Update 3-experiment-map-ucrania.js

--- public/js/3-experiment-map-ucrania.js
+++ public/js/3-experiment-map-ucrania.js
@@ -1435,7 +1435,7 @@ mapboxgl.accessToken = 'pk.eyJ1Ijoic3VjZW5kbyIsImEiOiJja3dvd243c3EwNzFhMm5sY3Byc
                 'type': 'Feature',
                 'properties': {
                     'message': 'Proyectil Impacta en Hryhorivka',
-                    'description': '<h3><strong>Proyectil Impacta en Hryhorivka</strong></h3><p>Un proyectil impacta cerca de una escuela en Hryhorivka, del distrito de Vasylkiv en la región de Dnipropetrovsk</p><p>08/03/2022 a las 3:52pm</p><img src="https://liveuamap.com/pics/2022/03/08/22403865_1.jpg"/>',
+                    'description': '<h3><strong>Proyectil Impacta en Hryhorivka</strong></h3><p>Un proyectil impacta cerca de una escuela en Hryhorivka, del distrito de Vasylkiv en la región de Dnipropetrovsk</p><p>08/03/2022 a las 3:52pm</p><img src="https://pbs.twimg.com/media/FNVdrX6XIAEPTp8?format=jpg&name=medium"/>',
                     'icon': 'http://www.ayudaparamiweb.com/icons/bombardeo-red.svg',
                     'iconSize': [40, 40]
                 },

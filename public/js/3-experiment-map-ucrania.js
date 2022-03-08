cad6afc2a2f81d0b81f0c7daab62cdc4c23fe5ba
sucendo <samuelug.pro@gmail.com>
2022-03-08 10:24:03 +0100

    Update 3-experiment-map-ucrania.js

--- public/js/3-experiment-map-ucrania.js
+++ public/js/3-experiment-map-ucrania.js
@@ -1637,6 +1637,68 @@ mapboxgl.accessToken = 'pk.eyJ1Ijoic3VjZW5kbyIsImEiOiJja3dvd243c3EwNzFhMm5sY3Byc
                     }
                 });
         */
+        
+        map.on('load', () => {
+        // Add an image to use as a custom marker
+        map.loadImage(
+            'https://docs.mapbox.com/mapbox-gl-js/assets/custom_marker.png',
+            (error, image) => {
+                if (error) throw error;
+                map.addImage('custom-marker', image);
+                // Add a GeoJSON source with 2 points
+                map.addSource('points', {
+                    'type': 'geojson',
+                    'data': {
+                        'type': 'FeatureCollection',
+                        'features': [
+                            {
+                                // feature for Mapbox DC
+                                'type': 'Feature',
+                                'geometry': {
+                                    'type': 'Point',
+                                    'coordinates': [
+                                        30.03238901390978, 50.913188059745586
+                                    ]
+                                },
+                                'properties': {
+                                    'title': 'Mapbox DC'
+                                }
+                            },
+                            {
+                                // feature for Mapbox SF
+                                'type': 'Feature',
+                                'geometry': {
+                                    'type': 'Point',
+                                    'coordinates': [30.414, 50.776]
+                                },
+                                'properties': {
+                                    'title': 'Mapbox SF'
+                                }
+                            }
+                        ]
+                    }
+                });
+
+                // Add a symbol layer
+                map.addLayer({
+                    'id': 'points',
+                    'type': 'symbol',
+                    'source': 'points',
+                    'layout': {
+                        'icon-image': 'custom-marker',
+                        // get the title name from the source's "title" property
+                        'text-field': ['get', 'title'],
+                        'text-font': [
+                            'Open Sans Semibold',
+                            'Arial Unicode MS Bold'
+                        ],
+                        'text-offset': [0, 1.25],
+                        'text-anchor': 'top'
+                    }
+                });
+            }
+        );
+    });
           
         // When a click event occurs on a feature in the places layer, open a popup at the
         // location of the feature, with description HTML from its properties.

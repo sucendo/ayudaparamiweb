d70a24d16ace8689ff3d828bb146f02acdaa68df
sucendo <samuelug.pro@gmail.com>
2022-08-09 13:06:05 +0200

    Update historia.js

--- public/js/experiments/historia/historia.js
+++ public/js/experiments/historia/historia.js
@@ -75,7 +75,7 @@ mapboxgl.accessToken = 'pk.eyJ1Ijoic3VjZW5kbyIsImEiOiJja3dvd243c3EwNzFhMm5sY3Byc
 
     // wait for the terrain and sky to load before starting animation
     map.on('load', () => {
-        const animationDuration = 80000;
+        const animationDuration = 800000;
         const cameraAltitude = 4000;
         // get the overall distance of each route so we can interpolate along them
         const routeDistance = turf.lineDistance(turf.lineString(targetRoute));

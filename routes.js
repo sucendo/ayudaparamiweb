f4e97a471ab68f1a4fef9b0a2e27db16fac7a6af
sucendo <samuelug.pro@gmail.com>
2026-04-09 09:26:15 +0200

    Add node/static runtime mode and gh-pages static build support

---
+++ routes.js
@@ -0,0 +1,29 @@
+module.exports = [
+  { path: '/', view: 'pages/index' },
+  { path: '/articulos', view: 'pages/articles' },
+  { path: '/herramientas', view: 'pages/tools' },
+  { path: '/experimentos', view: 'pages/experiments' },
+  { path: '/sucender', view: 'authors/sucender' },
+  { path: '/historia', view: 'historia/historia' },
+  { path: '/mapa-guerra-ucrania-rusia', view: 'experiments/0003-mapa-guerra-ucrania-rusia' },
+  { path: '/rusia-ataca-ucrania', view: 'experiments/0002-rusia-ataca-ucrania' },
+  { path: '/quantum-pacific-group', view: 'experiments/0001-quantum-pacific-group' },
+  {
+    path: '/calculo-posicion-provisional-pruebas-selectivas-comunidad-de-madrid-medico-familia-atencion-primaria-2019',
+    view: 'experiments/0004-comunidad-de-madrid-pruebas-selectivas-medico-familia-atencion-primaria-2019-2022'
+  },
+  { path: '/contador-caracteres-seo', view: 'tools/0002-contador-caracteres-seo' },
+  { path: '/conversor-binario', view: 'tools/0001-conversor-binario' },
+  { path: '/entornos-colaborativos', view: 'news/0021-entornos-colaborativos' },
+  { path: '/backlink-que-es-como-construir-red-de-enlaces', view: 'news/0013-backlink-que-es-como-construir-red-de-enlaces' },
+  { path: '/investigacion-palabras-clave', view: 'news/0012-investigacion-palabras-clave' },
+  { path: '/contenido-y-seo', view: 'news/0011-contenido-y-seo' },
+  { path: '/seo-on-page-aspectos-tecnicos', view: 'news/0010-seo-on-page-aspectos-tecnicos' },
+  { path: '/motores-de-busqueda', view: 'news/0009-motores-de-busqueda' },
+  { path: '/seo-que-es', view: 'news/0008-seo-que-es' },
+  { path: '/como-crear-una-pagina-web', view: 'news/0007-como-crear-una-pagina-web' },
+  { path: '/el-mundo-del-programador-web', view: 'news/0006-el-mundo-del-programador-web' },
+  { path: '/que-es-bluetooth', view: 'news/0005-que-es-bluetooth' },
+  { path: '/problemas-canon-digital-ecommerce', view: 'news/0004r-problemas-canon-digital-ecommerce' },
+  { path: '/codigo-traductor-google-blog', view: 'news/0002-codigo-traductor-google-blog' }
+];
